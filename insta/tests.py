db2ba76fa21fc4cba5ff86eef79bc1a58e1e85ef
Kennedy-karuri <kennkaruri99@gmail.com>
2021-01-19 23:21:37 +0300

    carried out tests

--- insta/tests.py
+++ insta/tests.py
@@ -6,18 +6,18 @@ from.models import Profile
 class ProfileTestClass(TestCase):
 
     def setUp(self):
-        self.Peris = Profile(name = 'Peris', profile_pic = 'image.jpg', bio='Always conected to my instagram')
-        self.Peris.save()
+        self.Kennedy = Profile(name = 'Kennedy', profile_pic = 'image.jpg', bio='studying at moringa')
+        self.Kennedy.save()
 
     def tearDown(self):
         Profile.objects.all().delete()
     
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.Peris, Profile))
+        self.assertTrue(isinstance(self.Kennedy, Profile))
 
     def test_save_method(self):
-        self.Peris.save_profile()
+        self.Kennedy.save_profile()
         name = Profile.objects.all()
 
     def test_delete_method(self):
@@ -27,21 +27,3 @@ class ProfileTestClass(TestCase):
 
 
 
-class PostTestCase(TestCase):
-
-    def setUp(self):
-        self.hiking = Profile(image= 'image.jpg', title = 'hiking', user='User')
-        self.hiking.save()
-
-
-    def tearDown(self):
-        Post.objects.all().delete()
-
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.hiking, Post))
-
-
-    def  test_save_method(self):
-        self.Peris.save_post()
-        tittle = Post.objects.all()

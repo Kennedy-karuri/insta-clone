cfdab60ab796df2742e109fc01f76d53e9873117
Kennedy-karuri <kennkaruri99@gmail.com>
2021-01-19 23:14:28 +0300

    modified models

--- insta/models.py
+++ insta/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 
 
-# Create your models here.
+
 class Profile(models.Model):
     name = models.CharField(max_length=30)
     profile_pic = models.ImageField(upload_to='new_post/', blank = 'true')
